f3fad1228f9489ac1d10cdc6469d7995fe85846c
ChenYiZh <546298815@qq.com>
2024-10-29 09:09:38 +0800

    添加一些实验性的代码

---
+++ Experiments/FantasyEngine/include/Windows/Defines.h
@@ -0,0 +1,11 @@
+//
+// Created by ChenYiZh on 2024/10/29.
+//
+
+#ifndef FANTASYENGINE_DEFINES_H
+#define FANTASYENGINE_DEFINES_H
+
+#define DLLEXPORT __declspec(dllexport)
+#define DLLIMPORT __declspec(dllimport)
+
+#endif //FANTASYENGINE_DEFINES_H
