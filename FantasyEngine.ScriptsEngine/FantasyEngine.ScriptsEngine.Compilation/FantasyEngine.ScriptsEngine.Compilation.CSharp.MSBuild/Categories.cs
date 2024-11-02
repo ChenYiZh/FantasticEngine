85a94f6a1ea6d4962e74ff0899c16ac71e363d6f
ChenYiZh <546298815@qq.com>
2024-11-02 14:29:22 +0800

    编译工具提交

---
+++ FantasyEngine.ScriptsEngine/FantasyEngine.ScriptsEngine.Compilation/FantasyEngine.ScriptsEngine.Compilation.CSharp.MSBuild/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyEngine.ScriptsEngine.Compilation.CSharpMSBuild
+{
+
+    internal class Categories
+    {
+        public static string COMPILATION = "CompilationCSharpMSBuid";
+    }
+}
