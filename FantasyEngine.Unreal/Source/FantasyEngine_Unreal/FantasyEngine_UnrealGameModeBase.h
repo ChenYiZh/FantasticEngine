8e2b3f22142118e9f92c5243931e32be62fb6948
ChenYiZh <546298815@qq.com>
2024-11-02 16:14:05 +0800

    创建UE项目案例

---
+++ FantasyEngine.Unreal/Source/FantasyEngine_Unreal/FantasyEngine_UnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FantasyEngine_UnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FANTASYENGINE_UNREAL_API AFantasyEngine_UnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
