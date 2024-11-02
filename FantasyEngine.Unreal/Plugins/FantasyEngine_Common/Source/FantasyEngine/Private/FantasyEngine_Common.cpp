97f7336c04a2fdc546b7a3fb9a1c1f685dd529ec
ChenYiZh <546298815@qq.com>
2024-11-02 18:01:13 +0800

    核心插件提交

---
+++ FantasyEngine.Unreal/Plugins/FantasyEngine_Common/Source/FantasyEngine/Private/FantasyEngine_Common.cpp
@@ -0,0 +1,16 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FantasyEngine_Common.h"
+
+IMPLEMENT_MODULE(FFantasyEngine_CommonModule, FantasyEngine_Common)
+
+void FFantasyEngine_CommonModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FFantasyEngine_CommonModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
