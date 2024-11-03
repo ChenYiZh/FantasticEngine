dca410e64833bd31e4bcca8c6f17b78297801496
ChenYiZh <546298815@qq.com>
2024-11-03 11:51:33 +0800

    虚拟摇杆提交

---
+++ FantasyEngine.Unreal/Plugins/FantasyEngine_VirtualInput/Source/FantasyEngine_VirtualInput/Private/FantasyEngine_VirtualInput.cpp
@@ -0,0 +1,14 @@
+﻿#include "FantasyEngine_VirtualInput.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_GAME_MODULE(FFantasyEngine_VirtualInputModule, FantasyEngine_VirtualInput);
+
+void FFantasyEngine_VirtualInputModule::StartupModule()
+{
+	IModuleInterface::StartupModule();
+}
+
+void FFantasyEngine_VirtualInputModule::ShutdownModule()
+{
+	IModuleInterface::ShutdownModule();
+}
