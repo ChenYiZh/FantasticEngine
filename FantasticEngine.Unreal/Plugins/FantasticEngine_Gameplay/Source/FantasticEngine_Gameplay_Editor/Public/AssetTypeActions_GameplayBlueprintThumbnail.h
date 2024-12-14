c6b85226120dc45150112d8368208c0db6e0ef07
ChenYiZh <546298815@qq.com>
2024-12-14 14:03:24 +0800

    删除多余代码库

--- FantasticEngine.Unreal/Plugins/FantasticEngine_Gameplay/Source/FantasticEngine_Gameplay_Editor/Public/AssetTypeActions_GameplayBlueprintThumbnail.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include "AssetTypeActions_BlueprintThumbnail.h"
-
-class FANTASTICENGINE_GAMEPLAY_EDITOR_API FAssetTypeActions_GameplayBlueprintThumbnail : public FAssetTypeActions_BlueprintThumbnail
-{
-public:
-	virtual const FSlateBrush* CheckAssetData(const TSubclassOf<UObject>& ParentClass) const override;
-};
